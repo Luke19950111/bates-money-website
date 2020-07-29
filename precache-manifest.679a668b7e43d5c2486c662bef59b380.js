6fe68b97027de2a767f1b2cc6f42e1d29fbfd33c
liukuai <937613925@qq.com>
2020-07-29 14:34:07 +0800

    deploy

---
+++ precache-manifest.679a668b7e43d5c2486c662bef59b380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060e8d50d6b0f071f45a",
+    "url": "/bates-money-website/css/app.604cf28f.css"
+  },
+  {
+    "revision": "39d23eea2cd074eb40154ee1f582b748",
+    "url": "/bates-money-website/index.html"
+  },
+  {
+    "revision": "060e8d50d6b0f071f45a",
+    "url": "/bates-money-website/js/app.998d9948.js"
+  },
+  {
+    "revision": "40111c338c16ec968404",
+    "url": "/bates-money-website/js/chunk-vendors.c9f9a11f.js"
+  },
+  {
+    "revision": "561cce6c319a5f73e049736d68e47215",
+    "url": "/bates-money-website/manifest.json"
+  },
+  {
+    "revision": "970d72f64d728521d48d6fa79bca49ec",
+    "url": "/bates-money-website/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bates-money-website/robots.txt"
+  }
+]);
